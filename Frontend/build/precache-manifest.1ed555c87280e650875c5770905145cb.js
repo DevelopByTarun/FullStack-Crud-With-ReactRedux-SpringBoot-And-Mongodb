4b1e82cfbdb9971921f82494aef66785a9ad5914
tarun <gemini86.tarun@gmail.com>
2019-01-28 23:14:36 +0530

    Applications

---
+++ Frontend/build/precache-manifest.1ed555c87280e650875c5770905145cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "33dad3bd20b5a9af85362bddd00ce091",
+    "url": "/static/media/top.33dad3bd.jpg"
+  },
+  {
+    "revision": "9ece4d098a13c5538fedfc39db98d60f",
+    "url": "/static/media/homy.9ece4d09.jpg"
+  },
+  {
+    "revision": "d94c375b634d3144520694aacd8ed4cf",
+    "url": "/static/media/cover4.d94c375b.jpg"
+  },
+  {
+    "revision": "2bd7e1311a8a9b040348458b67bb08a9",
+    "url": "/static/media/bann.2bd7e131.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78ef339b8b7fd9c70fc9",
+    "url": "/static/js/main.78ef339b.chunk.js"
+  },
+  {
+    "revision": "7144ff23f6a1aa2bf1aa",
+    "url": "/static/js/1.7144ff23.chunk.js"
+  },
+  {
+    "revision": "78ef339b8b7fd9c70fc9",
+    "url": "/static/css/main.879eaec8.chunk.css"
+  },
+  {
+    "revision": "7144ff23f6a1aa2bf1aa",
+    "url": "/static/css/1.2ab8e70a.chunk.css"
+  },
+  {
+    "revision": "a59a4dfc50ef3495f6401359bb2a37db",
+    "url": "/index.html"
+  }
+];
